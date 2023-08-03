cc113b8e20db733cdfefe0272f6a6da71b4168cc
Arjun V <arjunnarikkuni00@gmail.com>
2023-08-03 11:37:04 +0530

    ROOM DB ISSUE FIXED ->

--- app/src/main/java/com/posibolt/tmdbclient/data/db/TMDBDatabase.kt
+++ app/src/main/java/com/posibolt/tmdbclient/data/db/TMDBDatabase.kt
@@ -1,5 +1,7 @@
 package com.posibolt.tmdbclient.data.db
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.posibolt.tmdbclient.data.model.artist.Artist
 import com.posibolt.tmdbclient.data.model.movie.Movie
@@ -10,4 +12,23 @@ abstract class TMDBDatabase : RoomDatabase() {
     abstract fun movieDao() : MovieDao
     abstract fun artistDao() : ArtistDao
     abstract fun tvShowDao() : TvShowDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TMDBDatabase? = null
+        fun getInstance(context: Context): TMDBDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        TMDBDatabase::class.java,
+                        "subscribe_data_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
